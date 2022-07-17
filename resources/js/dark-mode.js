7b915677fa5831749b87081366d80947be894faa
CChase <christopher.chase13@gmail.com>
2022-07-16 20:21:26 -0700

    Added darkModeOff function and toggling modes

--- resources/js/dark-mode.js
+++ resources/js/dark-mode.js
@@ -1,9 +1,18 @@
 const content = document.getElementById('content');
+const toggle = document.getElementById('toggle');
 
-const darkModeOn = () => {
-    content.style.backgroundColor = 'hsla(341, 18%, 13%, 1)';
-    content.style.color = 'white';
+const darkModeOff = () => {
+    content.style.backgroundColor = 'white';
+    content.style.color = 'black';
+    toggle.removeEventListener('click', darkModeOff);
+    setTimeout(() => toggle.addEventListener('click', darkModeOn), 0);
 }
 
-const toggle = document.getElementById('toggle');
+const darkModeOn = (event) => {
+    content.style.backgroundColor = 'hsla(341, 18%, 13%, 1)';
+    content.style.color = 'white';
+    toggle.removeEventListener('click', darkModeOn);
+    setTimeout(() => toggle.addEventListener('click', darkModeOff), 0);
+}
+
 toggle.addEventListener('click', darkModeOn);
